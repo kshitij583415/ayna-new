cc1f19cd78a481860687db3ceaf16fa01c888a46
kshitij583415 <01fe21bcs314@kletech.ac.in>
2025-02-13 20:20:39 +0530

    Enabled WebSocket support

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::websocket",
+    config: {
+      enabled: true,
+    },
+  },
 ];
